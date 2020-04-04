08b8957874ca931f3d4a72196b39f383cfdaae7a
rajsran <61764796+rajsran@users.noreply.github.com>
2020-04-04 18:58:06 +1100

    Question 3
    OOP code

--- electronicDie.py
+++ electronicDie.py
@@ -1,105 +1,81 @@
 from sense_hat import SenseHat
-import time
-import random
+from time import sleep
 
-sense = SenseHat()
+class Smiley:
+    
+    def __init__(self):
+        self.sense= SenseHat()
+        self.y = (255,255,0)
+        self.r = (255, 0, 0)
+        self.b = (0, 0, 0)
+        self.w = (255,255,255)
 
-sense.clear()
+        self.image = [
+        self.y,self.y,self.y,self.y,self.y,self.y,self.y,self.y,
+        self.y,self.y,self.y,self.y,self.y,self.y,self.y,self.y,
+        self.y,self.y,self.y,self.y,self.y,self.y,self.y,self.y,
+        self.y,self.y,self.y,self.y,self.y,self.y,self.y,self.y,
+        self.y,self.y,self.y,self.y,self.y,self.y,self.y,self.y,
+        self.y,self.y,self.y,self.y,self.y,self.y,self.y,self.y,
+        self.y,self.y,self.y,self.y,self.y,self.y,self.y,self.y,
+        self.y,self.y,self.y,self.y,self.y,self.y,self.y,self.y
+        ]
+        self.sense.set_pixels(self.image)
 
-sense.show_message("Shake to roll dice")
-
-b = [0, 0, 0]
-g = [0, 255, 0]
-r = [255, 0, 0]
-
-one = [
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,g,g,b,b,b,
-b,b,b,g,g,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-]
-
-two = [
-b,b,b,b,b,b,b,b,
-b,g,g,b,b,b,b,b,
-b,g,g,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,g,g,b,b,
-b,b,b,b,g,g,b,b,
-b,b,b,b,b,b,b,b,
-]
-
-three = [
-g,g,b,b,b,b,b,b,
-g,g,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,g,g,b,b,b,
-b,b,b,g,g,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,g,g,
-b,b,b,b,b,b,g,g,
-]
-
-four = [
-b,b,b,b,b,b,b,b,
-b,g,g,b,b,g,g,b,
-b,g,g,b,b,g,g,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,g,g,b,b,g,g,b,
-b,g,g,b,b,g,g,b,
-b,b,b,b,b,b,b,b,
-]
-
-five = [
-g,g,b,b,b,b,g,g,
-g,g,b,b,b,b,g,g,
-b,b,b,b,b,b,b,b,
-b,b,b,g,g,b,b,b,
-b,b,b,g,g,b,b,b,
-b,b,b,b,b,b,b,b,
-g,g,b,b,b,b,g,g,
-g,g,b,b,b,b,g,g,
-]
-
-six = [
-r,r,b,b,b,b,r,r,
-r,r,b,b,b,b,r,r,
-b,b,b,b,b,b,b,b,
-r,r,b,b,b,b,r,r,
-r,r,b,b,b,b,r,r,
-b,b,b,b,b,b,b,b,
-r,r,b,b,b,b,r,r,
-r,r,b,b,b,b,r,r,
-]
-
-def roll_dice():
-    r = random.randint(1,6)
-    if r == 1:
-        sense.set_pixels(one)
-    elif r == 2:
-        sense.set_pixels(two)
-    elif r == 3:
-        sense.set_pixels(three)
-    elif r == 4:
-        sense.set_pixels(four)
-    elif r == 5:
-        sense.set_pixels(five)
-    elif r == 6:
-        sense.set_pixels(six)
+    def eyes(self):
+        self.sense.set_pixel(1,1,self.w)
+        self.sense.set_pixel(1,2,self.w)
+        self.sense.set_pixel(2,1,self.w)
+        self.sense.set_pixel(2,2,self.b)
+        self.sense.set_pixel(1,5,self.w)
+        self.sense.set_pixel(1,6,self.w)
+        self.sense.set_pixel(2,5,self.w)
+        self.sense.set_pixel(2,6,self.b)
+        
+    def smile(self):
+        self.sense.set_pixel(4,1,self.r)
+        self.sense.set_pixel(4,2,self.r)
+        self.sense.set_pixel(4,3,self.r)
+        self.sense.set_pixel(4,4,self.r)
+        self.sense.set_pixel(4,5,self.r)
+        self.sense.set_pixel(4,6,self.r)
+        self.sense.set_pixel(5,2,self.r)
+        self.sense.set_pixel(5,3,self.b)
+        self.sense.set_pixel(5,4,self.b)
+        self.sense.set_pixel(5,5,self.r)
+        self.sense.set_pixel(6,3,self.r)
+        self.sense.set_pixel(6,4,self.r)
+        
+    def sad(self):
+        self.sense.set_pixel(6,1,self.r)
+        self.sense.set_pixel(5,2,self.r)
+        self.sense.set_pixel(4,3,self.r)
+        self.sense.set_pixel(4,4,self.r)
+        self.sense.set_pixel(5,5,self.r)
+        self.sense.set_pixel(6,6,self.r)
+        
+    def haww(self):
+        self.sense.set_pixel(4,3,self.r)
+        self.sense.set_pixel(4,4,self.r)
+        self.sense.set_pixel(5,5,self.r)
+        self.sense.set_pixel(6,4,self.r)
+        self.sense.set_pixel(6,3,self.r)
+        self.sense.set_pixel(5,2,self.r)
+        self.sense.set_pixel(5,3,self.b)
+        self.sense.set_pixel(5,4,self.b)
+        
 
 while True:
-    x, y, z = sense.get_accelerometer_raw().values()
-
-    x = abs(x)
-    y = abs(y)
-    z = abs(z)
-   
-    if x > 1.4 or y > 1.4 or z > 1.4:
-        roll_dice()
-        time.sleep(1)
+    s1 = Smiley()
+    s1.eyes()
+    s1.smile()
+    sleep(3)
+    s2 = Smiley()
+    s2.eyes()
+    s2.sad()
+    sleep(3)
+    s3 = Smiley()
+    s3.eyes()
+    s3.haww()
+    sleep(3) 
+    
