1f62de30e90a5d5cac1ffc6ca835484d471bf50d
Nakhat-Yemen-Syeda <55049126+Nakhat-Yemen-Syeda@users.noreply.github.com>
2020-04-04 18:22:56 +1100

    question 02
    Final OOPS code

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -1,28 +1,58 @@
 import json
 from sense_hat import SenseHat
+from time import sleep
+from threading import Timer
+from threading import Thread
 
-f=open('/home/pi/Desktop/config.json' ,'r')
-s=f.read()
-temp=json.loads(s)
-sense = SenseHat()
-t = sense.get_temperature()
-t = round(t, 1)
-
-coldMax = temp["cold_max"]
-comfortableMin = temp["comfortable_min"]
-comfortableMax = temp["comfortable_max"]
-hotMin = temp["hot_min"]
-
-r = (255,0,0)
-g = (0,255,0)
-b = (0,0,255)
-message = "Temperature: " + str(t)
-
-if (t<=coldMax):
-    sense.show_message(message, scroll_speed=0.10, text_colour=b)
+class Temperature:
     
-elif (t>comfortableMin) and (t<comfortableMax):
-    sense.show_message(message, scroll_speed=0.10, text_colour=g)
+    def __init__(self):
+        self.sense = SenseHat()
+        self.temp = 0
+        self.r = (255,0,0)
+        self.g = (0,255,0)
+        self.b = (0,0,255)
+        
+        f=open('/home/pi/Desktop/assignment/config.json' ,'r')
+        s=f.read()
+        temp=json.loads(s)
+
+        self.coldMax = temp["cold_max"]
+        self.comfortableMin = temp["comfortable_min"]
+        self.comfortableMax = temp["comfortable_max"]
+        self.hotMin = temp["hot_min"]
+        
+    def setTemp(self):
+        self.temp = self.sense.get_temperature()-20.0
+        self.temp = round(self.temp, 1)
+        
+    def printMsg(self):
+        self.setTemp()
+        
+        message = "Temperature: " + str(self.temp)
+
+        if (self.temp<=self.coldMax):
+            self.sense.show_message(message, scroll_speed=0.10, text_colour=self.b)
+            
+        elif (self.temp>self.comfortableMin) and (self.temp<self.comfortableMax):
+            self.sense.show_message(message, scroll_speed=0.10, text_colour=self.g)
+            
+        else:
+            self.sense.show_message(message, scroll_speed=0.10, text_colour=self.r)
+            
+   
+    def count(self):
+           sleep(5)
+           self.sense.clear()
+           print("Here")
+           
+        
+currentTemp = Temperature()
+
+
+while True:
+    t1 = Thread(target = currentTemp.count)
+    t1.start()
+    sleep(2)
+    currentTemp.printMsg()
     
-else:
-    sense.show_message(message, scroll_speed=0.10, text_colour=r)
